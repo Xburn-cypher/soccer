afa1e9c02e6bd47838457a405357f60e0095922f
Xburn-cypher <68619750+Xburn-cypher@users.noreply.github.com>
2020-08-05 11:37:14 +0100

    Delete script2.js

--- script2.js
+++
@@ -1,7 +0,0 @@
-var c = 0;
-     function pop() {
-     				if ( c == 0 ){
-document.getElementById("box").style.display ="block";
-          c = 0;	}else{
-document.getElementById("box").style.display ="none";}
-        c = 2 }
