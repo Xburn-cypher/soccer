38833fb31539055c10f631707566c38a1bffa5dc
Xburn-cypher <68619750+Xburn-cypher@users.noreply.github.com>
2020-08-05 11:37:23 +0100

    Delete script1.js

--- script1.js
+++
@@ -1,7 +0,0 @@
-var b = 0;
-     function alert() {
-     				if ( b == 0 ){
-document.getElementById("box1").style.display ="block";
-          b = 0;	}else{
-document.getElementById("box1").style.display ="none";}
-        b = 2 }
