e6e0fef5f8d0111ed6809d640f48cd70905e6d20
Xburn-cypher <68619750+Xburn-cypher@users.noreply.github.com>
2020-08-05 11:32:52 +0100

    Soccer

---
+++ script1.js
@@ -0,0 +1,7 @@
+var b = 0;
+     function alert() {
+     				if ( b == 0 ){
+document.getElementById("box1").style.display ="block";
+          b = 0;	}else{
+document.getElementById("box1").style.display ="none";}
+        b = 2 }
---
+++ script2.js
@@ -0,0 +1,7 @@
+var c = 0;
+     function pop() {
+     				if ( c == 0 ){
+document.getElementById("box").style.display ="block";
+          c = 0;	}else{
+document.getElementById("box").style.display ="none";}
+        c = 2 }
